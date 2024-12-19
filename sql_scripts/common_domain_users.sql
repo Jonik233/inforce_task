262ec5baef4b381ddb2b66553d1d30cf5aa124f8
Vlad Mashtalier <vmashtaler5@gmail.com>
2024-12-19 07:22:49 +0200

    common domain users script documented

--- sql_scripts/common_domain_users.sql
+++ sql_scripts/common_domain_users.sql
@@ -1,8 +1,12 @@
 -- Find the user(s) with the most common email domain
 SELECT * FROM users 
-WHERE domain=(
-                SELECT domain FROM users
-                GROUP BY domain 
-                ORDER BY COUNT(*) DESC 
-                LIMIT 1
-            );
+WHERE domain = (
+    -- Subquery to find the most common email domain
+    SELECT domain FROM users
+    -- Group users by their email domain
+    GROUP BY domain
+    -- Order the domains by their frequency (count of users) in descending order
+    ORDER BY COUNT(*) DESC
+    -- Limit the result to the most frequent domain
+    LIMIT 1
+);
